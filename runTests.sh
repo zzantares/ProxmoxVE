de985ecba06e21c3bf0bdd3ca5cfbdccef6704ac
Alexey Ashurok <me@aotd.ru>
2014-07-09 11:06:35 +0700

    Fancy emacs codesniffer report style, fix PSR :3

--- runTests.sh
+++ runTests.sh
@@ -3,8 +3,8 @@
 composer install
 
 # Code style
-php vendor/bin/phpcs ./src/ --standard=PSR1 --encoding=utf-8 --report=full
-php vendor/bin/phpcs ./src/ --standard=PSR2 --encoding=utf-8 --report=full
+php vendor/bin/phpcs ./src/ --standard=PSR1 --encoding=utf-8 --report=emacs
+php vendor/bin/phpcs ./src/ --standard=PSR2 --encoding=utf-8 --report=emacs
 
 # PHPUnit tests
 php vendor/bin/phpunit --coverage-html .reports
